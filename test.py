09fd21227b34180ee7da37158198bf13acdea97a
Alexey Bolshakov <vokashlob@gmail.com>
2020-05-23 17:24:28 +0300

    Initial commit

---
+++ test.py
@@ -0,0 +1,6 @@
+
+
+def get_input():
+    user_input = input('Enter first letter for tower, e.g. "R" for right:')
+    return user_input
+
---
+++ towers_game.py
@@ -0,0 +1,59 @@
+from stack import Stack
+
+print("\nLet's play Towers of Hanoi!!")
+
+#Create the Stacks
+stacks = []
+left_stack = Stack('left')
+middle_stack = Stack('middle')
+right_stack = Stack('right')
+stacks.append(left_stack)
+stacks.append(middle_stack)
+stacks.append(right_stack)
+#Set up the Game
+num_disks = int(input('\nHow many disks do you want to play with?\n'))
+#print(num_disks)
+while num_disks < 3:
+  num_disks = int(input('Enter a number greater than or equal to 3\n'))
+
+for disk in range(num_disks, 0, -1):
+  left_stack.push(disk)
+
+num_optimal_moves = 2 ** num_disks -1
+print('The fastest you can solve this game is in {0} moves'.format(num_optimal_moves))
+
+#Get User Input
+def get_input():
+    choices = [stack.get_name()[0].upper() for stack in stacks]
+    while True:
+      for i in range(len(stacks)):
+        name = stacks[i].get_name()
+        letter = choices[i]
+        print('Enter {} for {}'.format(letter, name))
+      user_input = input('')
+      if user_input in choices:
+        for i in range(len(stacks)):
+          if user_input == choices[i]:
+            return stacks[i]
+
+#Play the Game
+num_user_moves = 0
+while right_stack.get_size() != num_disks:
+  print("\n\n\n...Current Stacks...")
+  for stack in stacks:
+    stack.print_items()
+  while True:
+    print("\nWhich stack do you want to move from?\n")
+    from_stack = get_input()
+    print("\nWhich stack do you want to move to?\n")
+    to_stack = get_input()
+    if from_stack.is_empty():
+      print("\n\nInvalid Move. Try Again")
+    elif to_stack.is_empty() or from_stack.peek() < to_stack.peek():
+      disk = from_stack.pop()
+      to_stack.push(disk)
+      num_user_moves += 1
+      break
+    else:
+      print("\n\nInvalid Move. Try Again")
+print(f"\n\nYou completed the game in {num_user_moves} moves, and the optimal number of moves is {num_optimal_moves}")
